4546b1d49cf3fcc16aea5ac7eab033fc8495b8f7
Jing Jie <jj@hireplace.com>
2020-05-12 16:52:59 +0800

    Updates

---
+++ precache-manifest.ed09cb67990d21293ab63ae1413645a5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "58b9b0f81cc496005cd3",
+    "url": "/cryptodash/static/js/runtime~main.58b9b0f8.js"
+  },
+  {
+    "revision": "bb000adbf0f0d7be1d1e",
+    "url": "/cryptodash/static/js/main.bb000adb.chunk.js"
+  },
+  {
+    "revision": "b8817699d2f322dd565e",
+    "url": "/cryptodash/static/js/1.b8817699.chunk.js"
+  },
+  {
+    "revision": "bb000adbf0f0d7be1d1e",
+    "url": "/cryptodash/static/css/main.73f66be1.chunk.css"
+  },
+  {
+    "revision": "625e2f9abfe6ef876d040b35121c88ca",
+    "url": "/cryptodash/index.html"
+  }
+];
